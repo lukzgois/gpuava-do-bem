47d6e33db99b70bca755a69305ac455d65921110
Lukz <lucaspgois@gmail.com>
2016-03-09 22:38:21 -0300

    Created the cards and table styles
    https://trello.com/c/SjsYmiqC/8-cadastro-de-administradores
    
    Created the card basic style and table appearance.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.main')
 
 @section('content')
+    <h1>Cadastro de Usuários</h1>
+
+
+    <div class="card">
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>teste</td>
+                    <td>teste</td>
+                    <td class="table__actions">
+                        <a href="#">
+                            <i class="fa fa-pencil fa-fw"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa fa-trash fa-fw"></i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
 @endsection
